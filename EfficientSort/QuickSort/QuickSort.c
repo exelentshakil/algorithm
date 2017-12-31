9ab391b53fc78f49197357c57beb1a8c4b5e97b1
Shakil Ahmed <exelentshakor@gmail.com>
2017-12-31 12:36:44 +0600

    Completed QuickSort Algorithm

--- EfficientSort/QuickSort/QuickSort.c
+++ EfficientSort/QuickSort/QuickSort.c
@@ -1,17 +1,85 @@
 #include<stdio.h>
 #include<stdlib.h> // malloc, free, qsort, bsearch
 
-/*
-    Quick Sort
+//
+//  QuickSort.c
+//  QuickSort
+//
+//  Created by Shakil Ahmed on 12/31/2017.
+//  Copyright © 2017 Shakil Empire. All rights reserved.
+//
 
-*/
+// Swap
+void Swap(
+              int *_pre,
+              int *_next
+        ){
+              int _accumulator = *_pre;
+              *_pre = *_next;
+              *_next = _accumulator;
+        }
 
+// Partition
+int partition(
+                  int *A,
+                  int _start,
+                  int _end
+              ){
+                  int _pIndex, _pivot, _i;
 
+                  // Pivot
+                  _pivot = A[_end];
 
+                  // Partition index
+                  _pIndex = _start;
+
+                  for(_i = _start; _i<_end; _i++)
+                  {
+                     if(A[_i] < _pivot)
+                     {
+                        Swap(&A[_i], &A[_pIndex]);
+                        _pIndex++;
+                     }
+                  }
+
+                  Swap(&A[_pIndex], &A[_end]);
+
+                  // Return the pIndex
+                  return _pIndex;
+              }
+
+// Quick Sort
+
+void QuickSort(
+                    int *A,
+                    int _start,
+                    int _end
+               ){
+
+                    if(_start < _end)
+                    {
+                        int _pIndex = partition(A, _start, _end);
+                        QuickSort(A, _start, _pIndex-1);
+                        QuickSort(A, _pIndex+1, _end);
+                    }
+
+               }
 
 int main()
 {
+    int i;
+    int A[] = {8,9,7,5,3,4};
 
+    int size=sizeof(A) / sizeof(A[0]);
+
+    // Quick Sort
+
+    QuickSort(A, 0, size-1);
+
+    for(i=0; i<size; i++)
+    {
+        printf("%d ", A[i]);
+    }
 
 
     return 0;
